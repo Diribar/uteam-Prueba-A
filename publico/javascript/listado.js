a2fc3441ff3cdcbff42906be7f193825456e5795
diribar <diegoiribarren2015@gmail.com>
2024-11-25 19:32:21 -0300

    Avance del proyecto

--- publico/javascript/listado.js
+++ publico/javascript/listado.js
@@ -2,12 +2,12 @@
 window.addEventListener("load", () => {
 	// DOM
 	const DOM = {
-		eliminars: document.querySelectorAll("tbody .eliminar i"),
-		marvel_ids: document.querySelectorAll("tbody .marvel_id"),
-		inputs: document.querySelectorAll("tbody .input"),
 		trs: document.querySelectorAll("tbody tr"),
+		editars: document.querySelectorAll("tbody .editar"),
+		marvel_ids: document.querySelectorAll("tbody .marvel_id"),
+		inputs: document.querySelectorAll("tbody input"),
+		eliminars: document.querySelectorAll("tbody .eliminar i"),
 	};
-
 
 	// Eventos - eliminar
 	DOM.eliminars.forEach((eliminar, fila) => {
@@ -19,6 +19,28 @@ window.addEventListener("load", () => {
 			let datos = "/?marvel_id=" + DOM.marvel_ids[fila].innerHTML;
 
 			fetch("/api/eliminar" + datos);
+		});
+	});
+	// Eventos - editar
+	DOM.editars.forEach((editar, fila) => {
+		editar.addEventListener("click", () => {
+			// Si el ícono está inactivo interrumpe la función
+			if (editar.className.includes("inactivo")) return;
+			editar.classList.replace("fa-circle-check", "fa-pen");
+
+			// Obtiene los datos
+			let datos = "";
+			for (let i = 0; i < 3; i++) {
+				// Consigue los datos
+				const input = DOM.inputs[i + fila * 3];
+				if (!datos) datos += "?";
+				else if (input.value == "s/d") continue;
+				else datos += "&";
+				datos += input.name + "=" + input.value;
+
+				// Envía los datos
+				fetch("/api/editar" + datos);
+			}
 		});
 	});
 });

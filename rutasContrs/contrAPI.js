5bd0b626e492aac964322db457579d11a2f458da
diribar <diegoiribarren2015@gmail.com>
2024-11-25 18:56:44 -0300

    Avance del proyecto

--- rutasContrs/contrAPI.js
+++ rutasContrs/contrAPI.js
@@ -5,11 +5,7 @@ module.exports = {
 	agregar: (req, res) => {
 		// Variables
 		const datos = req.query;
-
-		// Obtiene la BD
-		const rutaNombre = path.join(__dirname, "../auxiliar/comics.json");
-		const BD = fs.readFileSync(rutaNombre, "utf8");
-		let info = JSON.parse(BD);
+		let info = obtieneComicsDeBd()
 
 		// Agrega el nuevo registro y lo guarda
 		info.push(datos);
@@ -22,14 +18,10 @@ module.exports = {
 	eliminar: (req, res) => {
 		// Variables
 		const {marvel_id} = req.query;
-
-		// Obtiene la BD
-		const rutaNombre = path.join(__dirname, "../auxiliar/comics.json");
-		const BD = fs.readFileSync(rutaNombre, "utf8");
-		let info = JSON.parse(BD);
+		let info = obtieneComicsDeBd()
 
 		// Elimina el registro y lo guarda
-		info = info.filter((n) => n.marvel_id != "428");
+		info = info.filter((n) => n.marvel_id != marvel_id);
 		fs.writeFileSync(rutaNombre, JSON.stringify(info));
 
 		// Fin
@@ -38,8 +30,25 @@ module.exports = {
 	editar: (req, res) => {
 		// Variables
 		const datos = req.query;
+		let info = obtieneComicsDeBd()
+
+		// Actualiza el registro y lo guarda
+		info = info.filter((n) => n.marvel_id != marvel_id);
+		info.push(datos);
+		info.sort((a, b) => (a.titulo < b.titulo ? -1 : 1)); // los ordena por id
+		fs.writeFileSync(rutaNombre, JSON.stringify(info));
 
 		// Fin
 		return res.json();
 	},
 };
+
+const obtieneComicsDeBd=()=>{
+	// Obtiene la BD
+	const rutaNombre = path.join(__dirname, "../auxiliar/comics.json");
+	const BD = fs.readFileSync(rutaNombre, "utf8");
+	let info = JSON.parse(BD);
+
+	// Fin
+	return info
+}

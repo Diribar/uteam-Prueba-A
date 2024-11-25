bda2b4581dccb38fa462834ed84d500d3319e676
diribar <diegoiribarren2015@gmail.com>
2024-11-25 11:06:43 -0300

    Avance del proyecto

---
+++ app.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// Servidor
+global.express = require("express");
+const app = express();
+app.listen(80, () => console.log("Servidor funcionando..."));
+
+// Requires
+global.path = require("path");
+
+// Carpeta de formatos
+const carpetaFormatos = path.join(__dirname, "publico/formatos");
+app.use("/formatos", express.static(carpetaFormatos));
+const carpetaJS = path.join(__dirname, "publico/javascript");
+app.use("/javascript", express.static(carpetaJS));
+
+// Vistas
+app.set("view engine", "ejs"); // Terminación de los archivos de vista
+app.set("views", ["./vistas"]);
+
+// Ruteador
+// app.use("/", require("./rutasContrs/ruta"));

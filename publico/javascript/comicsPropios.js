4e3b3cb996400bcbd9addb82264dac0fb9bea4c0
diribar <diegoiribarren2015@gmail.com>
2024-11-26 08:58:25 -0300

    Avance del proyecto

--- publico/javascript/comicsPropios.js
+++ publico/javascript/comicsPropios.js
@@ -59,6 +59,7 @@ window.addEventListener("load", () => {
 			fetch("/api/editar" + datos);
 		});
 	});
+	// Activa/ inactiva el botón confirmar
 	DOM.inputs.forEach((input, i) => {
 		input.addEventListener("input", () => {
 			// Averigua la fila
